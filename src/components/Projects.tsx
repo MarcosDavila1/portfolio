3a6a2c9f834b854b596ce896eb319075c4fbbadc
Marcos Davila <55298236+MarcosDavila1@users.noreply.github.com>
2022-04-24 18:38:01 -0300

    projectsDone

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from "swiper";
+import { Link } from 'react-router-dom';
 import appclima from '../images/appclima.png'
 import blogcafe from '../images/blogcafe.png'
 import countrys from '../images/countrys.png'
@@ -9,41 +11,71 @@ import tesla from '../images/tesla.png'
 import zooper from '../images/zooper.png'
 
 import 'swiper/css';
+import "swiper/css/navigation";
 import '../styles/projects.css'
 
 function Projects() {
   return (
     <Swiper
-      spaceBetween={0}
-      slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
+        preloadImages={true}
+        loop={true}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
     >
-      <SwiperSlide tag='div'>
+      <SwiperSlide className='slideContent'>
+          <div className='textContent'>
+              <h1>ZOOPER TRIVIA</h1>
+              <a href='https://www.zoopertrivia.com/' target='_blank' className='linkVisit'>Visit</a>
+          </div>
           <img className='imgProject' src={zooper} alt='zooperTrivia'/>
       </SwiperSlide>
 
-      <SwiperSlide>
-          <img className='imgProject' src={ecommerce} alt='zooperTrivia'/>
-      </SwiperSlide>
-
-      <SwiperSlide>
-          <img className='imgProject' src={countrys} alt='zooperTrivia'/>
-      </SwiperSlide>
-
-      <SwiperSlide>
+      <SwiperSlide className='slideContent'>
+          <div className='textContent'>
+              <h1>FAST TASKS</h1>
+              <a href='https://fasttasks.netlify.app/' target='_blank' className='linkVisit'>Visit</a>
+          </div>
           <img className='imgProject' src={taskapp} alt='zooperTrivia'/>
       </SwiperSlide>
 
-      <SwiperSlide>
+      <SwiperSlide className='slideContent'>
+          <div className='textContent'>
+              <h1>ECOMMERCE</h1>
+              <a href='https://github.com/MarcosDavila1/productspage' target='_blank' className='linkVisit'>Visit</a>
+          </div>
+          <img className='imgProject' src={ecommerce} alt='zooperTrivia'/>
+      </SwiperSlide>
+
+      <SwiperSlide className='slideContent'>
+          <div className='textContent'>
+              <h1>COUNTRY APP</h1>
+              <a href='https://github.com/MarcosDavila1/PI-Country' target='_blank' className='linkVisit'>Visit</a>
+          </div>
+          <img className='imgProject' src={countrys} alt='zooperTrivia'/>
+      </SwiperSlide>
+
+      <SwiperSlide className='slideContent'>
+          <div className='textContent'>
+              <h1>TESLA</h1>
+              <a href='https://github.com/MarcosDavila1/Tesla-Page' target='_blank' className='linkVisit'>Visit</a>
+          </div>
           <img className='imgProject' src={tesla} alt='zooperTrivia'/>
       </SwiperSlide>
 
-      <SwiperSlide>
+      <SwiperSlide className='slideContent'>
+          <div className='textContent'>
+              <h1>WEATHER APP</h1>
+              <a href='https://github.com/MarcosDavila1/App-Clima' target='_blank' className='linkVisit'>Visit</a>
+          </div>
           <img className='imgProject' src={appclima} alt='zooperTrivia'/>
       </SwiperSlide>
 
-      <SwiperSlide>
+      <SwiperSlide className='slideContent'>
+          <div className='textContent'>
+              <h1>BLOG CAFE</h1>
+              <a href='https://github.com/MarcosDavila1' target={'_blank'} className='linkVisit'>Visit</a>
+          </div>
           <img className='imgProject' src={blogcafe} alt='zooperTrivia'/>
       </SwiperSlide>
       ...
