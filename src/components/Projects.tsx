27aef8e67c3734f238550624f96ce5e7d725b8bb
Marcos Davila <55298236+MarcosDavila1@users.noreply.github.com>
2022-04-24 19:16:14 -0300

    cardEffects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -24,57 +24,71 @@ function Projects() {
         className="mySwiper"
     >
       <SwiperSlide className='slideContent'>
-          <div className='textContent'>
-              <h1>ZOOPER TRIVIA</h1>
-              <a href='https://www.zoopertrivia.com/' target='_blank' className='linkVisit'>Visit</a>
-          </div>
-          <img className='imgProject' src={zooper} alt='zooperTrivia'/>
+            <div className='textContent'>
+                <Fade>
+                    <h1>ZOOPER TRIVIA</h1>
+                    <a href='https://www.zoopertrivia.com/' target='_blank' className='linkVisit'>Visit</a>
+                </Fade>
+            </div>
+            <img className='imgProject' src={zooper} alt='zooperTrivia'/>
+      </SwiperSlide>
+
+      <SwiperSlide className='slideContent'>
+            <div className='textContent'>
+                <Fade>
+                    <h1>FAST TASKS</h1>
+                    <a href='https://fasttasks.netlify.app/' target='_blank' className='linkVisit'>Visit</a>
+                </Fade>
+            </div>
+            <img className='imgProject' src={taskapp} alt='zooperTrivia'/>
       </SwiperSlide>
 
       <SwiperSlide className='slideContent'>
           <div className='textContent'>
-              <h1>FAST TASKS</h1>
-              <a href='https://fasttasks.netlify.app/' target='_blank' className='linkVisit'>Visit</a>
-          </div>
-          <img className='imgProject' src={taskapp} alt='zooperTrivia'/>
-      </SwiperSlide>
-
-      <SwiperSlide className='slideContent'>
-          <div className='textContent'>
-              <h1>ECOMMERCE</h1>
-              <a href='https://github.com/MarcosDavila1/productspage' target='_blank' className='linkVisit'>Visit</a>
+              <Fade>
+                <h1>ECOMMERCE</h1>
+                <a href='https://github.com/MarcosDavila1/productspage' target='_blank' className='linkVisit'>Visit</a>
+              </Fade>
           </div>
           <img className='imgProject' src={ecommerce} alt='zooperTrivia'/>
       </SwiperSlide>
 
       <SwiperSlide className='slideContent'>
           <div className='textContent'>
-              <h1>COUNTRY APP</h1>
-              <a href='https://github.com/MarcosDavila1/PI-Country' target='_blank' className='linkVisit'>Visit</a>
+              <Fade>
+                <h1>COUNTRY APP</h1>
+                <a href='https://github.com/MarcosDavila1/PI-Country' target='_blank' className='linkVisit'>Visit</a>
+              </Fade>
           </div>
           <img className='imgProject' src={countrys} alt='zooperTrivia'/>
       </SwiperSlide>
 
       <SwiperSlide className='slideContent'>
           <div className='textContent'>
-              <h1>TESLA</h1>
-              <a href='https://github.com/MarcosDavila1/Tesla-Page' target='_blank' className='linkVisit'>Visit</a>
+              <Fade>
+                <h1>TESLA</h1>
+                <a href='https://github.com/MarcosDavila1/Tesla-Page' target='_blank' className='linkVisit'>Visit</a>
+              </Fade>
           </div>
           <img className='imgProject' src={tesla} alt='zooperTrivia'/>
       </SwiperSlide>
 
       <SwiperSlide className='slideContent'>
           <div className='textContent'>
-              <h1>WEATHER APP</h1>
-              <a href='https://github.com/MarcosDavila1/App-Clima' target='_blank' className='linkVisit'>Visit</a>
+              <Fade>
+                <h1>WEATHER APP</h1>
+                <a href='https://github.com/MarcosDavila1/App-Clima' target='_blank' className='linkVisit'>Visit</a>
+              </Fade>
           </div>
           <img className='imgProject' src={appclima} alt='zooperTrivia'/>
       </SwiperSlide>
 
       <SwiperSlide className='slideContent'>
           <div className='textContent'>
-              <h1>BLOG CAFE</h1>
-              <a href='https://github.com/MarcosDavila1' target={'_blank'} className='linkVisit'>Visit</a>
+              <Fade>
+                <h1>BLOG CAFE</h1>
+                <a href='https://github.com/MarcosDavila1' target={'_blank'} className='linkVisit'>Visit</a>
+              </Fade>
           </div>
           <img className='imgProject' src={blogcafe} alt='zooperTrivia'/>
       </SwiperSlide>
