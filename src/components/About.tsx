a5a24e20e35732e8f7658f55b3f57f7384930aa0
Marcos Davila <55298236+MarcosDavila1@users.noreply.github.com>
2022-04-25 17:53:06 -0300

    aboutText

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,15 +19,18 @@ function About() {
         <Fade>
           <h1>About Me</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Suspendisse bibendum odio nisl, nec consectetur ante vulputate a. 
-            Donec posuere ex porttitor porta auctor. Aliquam pharetra ac arcu sed sagittis. 
-            Sed sapien mi, elementum sit amet finibus eu, tristique id erat. 
+            Since I was a kid, I was into technology and games. 
+            How fast the technolgies world changes is what 
+            I liked the most and encouraged me to continue learning everyday in order 
+            to be always up to date with the latest technologies, projects and news. 
           </p>
           <p>
-            Nam ipsum velit, sagittis at odio sit amet, congue dignissim justo. 
-            Nulla mollis est non erat sodales, at sollicitudin ante sollicitudin. 
-            Etiam maximus lobortis tortor. 
+            I developed all my skills at Henry Bootcamp and at Udemy. 
+            I learned and upgraded my abilities in Javascript, React, Redux, NodeJs, 
+            Express, GIT, Firebase, Socket.io, Css and plenty of other soft skills, 
+            while I worked in different projects. 
+            Also I have experience working with agile methodologies as Scrum.
+            I am currently improving my competencies in Typescript and Amazon Web Services. 
           </p>
         </Fade>
         <div className='containerBtnAbout'>
