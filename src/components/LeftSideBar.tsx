28f298a69cfed6648fad5261689779b9f31286f0
Marcos Davila <55298236+MarcosDavila1@users.noreply.github.com>
2022-05-07 15:40:08 -0300

    logoLink

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/leftSideBar.css'
 
 function LeftSideBar() {
   return (
     <div className='containerLeftSideBar'>
-      <a href='#'>
-        <h1>MD</h1>
-      </a>
+      <Link className='mdLogo' to='/'>MD</Link>
 
       <div className='containerRedes'>   
-        <a className='links' href='https://github.com/MarcosDavila1' target='_blank'>    
+        <a className='links' href='https://github.com/MarcosDavila1' target='_blank' rel="noreferrer">    
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt='github'/>
         </a> 
-        <a className='links' href='https://www.linkedin.com/in/marcosdavila2/' target='_blank'>
+        <a className='links' href='https://www.linkedin.com/in/marcosdavila2/' target='_blank' rel="noreferrer">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkedin'/>   
         </a>
       </div>
